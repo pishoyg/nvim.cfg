77fd80a58f920b505495b9d504cc05afb5ee44ee
Pimyn Girgis <pimyn@google.com>
2025-10-06 13:55:32 +0000

    remove noice and use default theme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,47 +1,4 @@
-return { -- Theme
-  {      --
-    'folke/noice.nvim',
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      lsp = {
-        -- override markdown rendering so that cmp and other plugins use Treesitter
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-        signature = {
-          enabled = false,
-        }
-      },
-      presets = {
-        bottom_search = true,
-        long_message_to_split = true,
-        inc_rename = true,
-      },
-      notify = {
-        enabled = false
-      }
-    },
-  },
-  {
-    'rcarriga/nvim-notify'
-  },
-
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      flavour = "mocha",
-      integrations = {
-        notify = true,
-      }
-    }
-  },
+return {
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = "VeryLazy",
@@ -72,30 +29,15 @@ return { -- Theme
       },
       sections = {
         lualine_x = {
-          -- {
-          --   require("noice").api.status.message.get_hl,
-          --   cond = require("noice").api.status.message.has,
-          -- },
-          {
-            require("noice").api.status.command.get,
-            cond = require("noice").api.status.command.has,
-            color = { fg = "#ff9e64" },
-          },
-          {
-            require("noice").api.status.mode.get,
-            cond = require("noice").api.status.mode.has,
-            color = { fg = "#ff9e64" },
-          },
-          {
-            require("noice").api.status.search.get,
-            cond = require("noice").api.status.search.has,
-            color = { fg = "#ff9e64" },
-          },
+          'status',
           'encoding',
           'fileformat',
           'filetype'
         },
       },
     },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    }
   },
 }
